804abc1ad2972a68955039422cceedc417098fa8
SyTuck <jptuck@yahoo.com>
2019-11-08 14:28:00 -0500

    road alignment fixed(ish)
    simplified, and fixed the roads (self-aligning)
    
    btw this should work by just placing the cities. Once the roads are created, they will map themselves out based on their start and end points

--- The Big White Freightliners/Assets/Prefabs/Highway/HighwayDetails.cs
+++ The Big White Freightliners/Assets/Prefabs/Highway/HighwayDetails.cs
@@ -12,15 +12,18 @@ public class HighwayDetails : MonoBehaviour
     void Start()
     {
         this.transform.position = startPoint.position;
+        this.transform.position.Set(startPoint.position.x, startPoint.position.y - 0.2f, startPoint.position.z);
+        this.transform.LookAt(endPoint, Vector3.up);
+        //this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y - 90.0f, this.transform.rotation.z,1);
 
-        Quaternion rotit = new Quaternion();
-        float rotAngle = Mathf.Atan((startPoint.position.z - endPoint.position.z) / (startPoint.position.x - endPoint.position.x)) * 180 / Mathf.PI;
-        if (rotAngle > 180)
-        {
-            rotAngle -= 180;
-        }
-        rotit.Set(this.transform.rotation.x, rotAngle,this.transform.rotation.z, 1);
-        this.transform.rotation = rotit;
+//        Quaternion rotit = new Quaternion();
+//        float rotAngle = Mathf.Atan((startPoint.position.z - endPoint.position.z) / (startPoint.position.x - endPoint.position.x)) * 180 / Mathf.PI;
+//        if (rotAngle > 180)
+//        {
+//            rotAngle -= 180;
+//        }
+//        rotit.Set(this.transform.rotation.x, rotAngle,this.transform.rotation.z, 1);
+//        this.transform.rotation = rotit;
     }
 
     // Update is called once per frame
