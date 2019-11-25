fc35dc4df7e41c676c616b26785e40fb368ae902
Tranquil <44653127+mjes87@users.noreply.github.com>
2019-11-25 16:19:43 -0500

    Added GameManager
    Setup basic skeleton for GameManager with GameStates. Also added Intro Scene for GameState debugging purposes.

---
+++ The Big White Freightliners/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { INTRO, MENU, GAMEPLAY }
+
+public delegate void OnStateChangeHandler();
+
+public class GameManager : MonoBehaviour
+{
+    protected GameManager() { }
+
+    private static GameManager instance = null;
+    public event OnStateChangeHandler OnStateChange;
+
+    public GameState gameState { get; private set; }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (GameManager.instance == null)
+            {
+                DontDestroyOnLoad(GameManager.instance);
+                GameManager.instance = new GameManager();
+            }
+            return GameManager.instance;
+        }
+    }
+
+    public void SetGameState(GameState state)
+    {
+        this.gameState = state;
+        OnStateChange();
+    }
+
+    public void OnApplicationQuit()
+    {
+        GameManager.instance = null;
+    }
+}
