cb839e4cf1ae3d5d8cab235e3b81d947245dde13
Mike Sullivan <mjes87@gmail.com>
2019-11-28 15:04:27 -0500

    Fixed Broken Prefabs
    Accident and Construction prefabs lost script / images.

---
+++ The Big White Freightliners/Assets/Scripts/Zoom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    float cameraDistanceMax = 20f;
+    float cameraDistanceMin = 5f;
+    float cameraDistance = 10f;
+    float scrollSpeed = 0.5f;
+
+    void Update()
+    {
+        cameraDistance += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        cameraDistance = Mathf.Clamp(cameraDistance, cameraDistanceMin, cameraDistanceMax);
+
+        // set camera position
+    }
+}
