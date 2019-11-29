536c73a2aad13c45e9bbfc17f067b07bdfcbf1d0
Mike Sullivan <mjes87@gmail.com>
2019-11-28 19:50:01 -0500

    Added UI Panels
    Bottom and Side UI Panels added with 2 Buttons.

---
+++ The Big White Freightliners/Assets/Scripts/UI/Loads.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loads : MonoBehaviour
+{
+    public GameObject SidePanel;
+    int counter;
+
+    public void togglePanel()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            SidePanel.gameObject.SetActive(true);
+        }
+        else
+        {
+            SidePanel.gameObject.SetActive(false);
+        }
+    }
+}
