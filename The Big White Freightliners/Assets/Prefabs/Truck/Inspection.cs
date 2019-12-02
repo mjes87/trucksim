43cf695fc4227f97ea0d7600d4b39c4dcc155a09
SyTuck <44281354+SyTuck@users.noreply.github.com>
2019-12-02 16:22:20 -0500

    more inspector stuff

--- The Big White Freightliners/Assets/Prefabs/Truck/Inspection.cs
+++ The Big White Freightliners/Assets/Prefabs/Truck/Inspection.cs
@@ -8,8 +8,8 @@ public class Inspection : MonoBehaviour
     private int hourlyRate = 96;
     private float HourlyDuration = 0.5f;
     private int lastHour;
-
     private float waitTime;
+    private bool beingInspected;
 
     const int NORMISNPECTION = 7;
     // Start is called before the first frame update
@@ -21,16 +21,21 @@ public class Inspection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int hr = FindObjectOfType<GameTime>().GetHour();
-        if (lastHour != hr)                                                             //every hour check for an accident
+        if (!beingInspected)
         {
-            lastHour = hr;
-
-            if (Random.Range(0, hourlyRate) == 0)
+            int hr = FindObjectOfType<GameTime>().GetHour();
+            if (lastHour != hr)                                                             //every hour check for an accident
             {
+                lastHour = hr;
+
+                if (Random.Range(0, hourlyRate) == 0)
+                {
+                    GameObject inspect = Instantiate(GameObject.Find("Inspector").gameObject) as GameObject;
+                    inspect.transform.position = this.gameObject.transform.position;
+                    inspect.GetComponent<MeshRenderer>().enabled = true;
+                }
 
             }
-
         }
     }
 }
